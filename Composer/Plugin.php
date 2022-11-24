9aef01218cf42208389873802ff2fa78a62278f2
AshMcDev <36491774+AshMcDev@users.noreply.github.com>
2022-11-24 23:44:40 +0000

    actualy use my installer
    i did a script kidy,

--- Composer/Plugin.php
+++ Composer/Plugin.php
@@ -4,7 +4,7 @@ class MyPlugin implements \Composer\Plugin\PluginInterface
 {
 	public function activate(\Composer\Composer $composer, \Composer\IO\IOInterface $io)
     {
-        $installer = new TemplateInstaller($io, $composer);
+        $installer = new MyInstaller($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
 }
